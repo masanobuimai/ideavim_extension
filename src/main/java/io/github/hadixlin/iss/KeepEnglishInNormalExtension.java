ce6446f22323215143fb048951c64bc886f482fb
masanobu.imai <masanobu.imai@gmail.com>
2018-05-15 17:59:16 +0900

    Windows専用で，keep-english-in-normalだけ実装してみた。 ...-and-restore-in-insertは，oコマンドとか拾えなかったりと微妙なので実装してない。

--- src/main/java/io/github/hadixlin/iss/KeepEnglishInNormalExtension.java
+++ src/main/java/io/github/hadixlin/iss/KeepEnglishInNormalExtension.java
@@ -1,15 +1,31 @@
 package io.github.hadixlin.iss;
 
+import com.google.common.collect.ImmutableSet;
+import com.intellij.openapi.command.CommandAdapter;
+import com.intellij.openapi.command.CommandEvent;
+import com.intellij.openapi.command.CommandListener;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.maddyhome.idea.vim.extension.VimExtension;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+import java.util.Optional;
+import java.util.Set;
+
 /**
- * Created by hadix on 09/04/2017.
+ * Created by hadix on 31/03/2017.
  */
-public class KeepEnglishInNormalExtension
-        extends KeepEnglishInNormalAndRestoreInInsertExtension {
+public class KeepEnglishInNormalExtension implements VimExtension {
+
+    private static final Set<String> SWITCH_TO_ENGLISH_COMMAND_NAMES
+            = ImmutableSet.of("Vim Exit Insert Mode");
+
+    private CommandListener exitInsertModeListener;
 
     public KeepEnglishInNormalExtension() {
-        super(false);
     }
 
     @NotNull
@@ -17,4 +33,43 @@ public class KeepEnglishInNormalExtension
     public String getName() {
         return "keep-english-in-normal";
     }
+
+    @Override
+    public void init() {
+        if (exitInsertModeListener == null) {
+            this.exitInsertModeListener = exitInsertModeListener();
+        }
+        CommandProcessor.getInstance().addCommandListener(this.exitInsertModeListener);
+    }
+
+    @NotNull
+    private CommandListener exitInsertModeListener() {
+        return new CommandAdapter() {
+            @Override
+            public void beforeCommandFinished(CommandEvent commandEvent) {
+                String commandName = commandEvent.getCommandName();
+                if (StringUtils.isBlank(commandName)) {
+                    return;
+                }
+                if (SWITCH_TO_ENGLISH_COMMAND_NAMES.contains(commandName)) {
+                    Editor editor = FileEditorManager.getInstance(commandEvent.getProject()).getSelectedTextEditor();
+                    Optional.ofNullable(editor.getComponent())
+                            .ifPresent(c -> {
+                                c.enableInputMethods(false);
+                                c.getInputContext().setCompositionEnabled(false);
+                            });
+                }
+            }
+        };
+    }
+
+    @Override
+    public void dispose() {
+        if (exitInsertModeListener == null) {
+            return;
+        }
+        CommandProcessor.getInstance().removeCommandListener(exitInsertModeListener);
+        exitInsertModeListener = null;
+    }
+
 }
